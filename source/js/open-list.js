e9f740a53c763aadec114824b1cecdbd27628cdd
vbratsyun <vbratsyun@yandex.ru>
2024-06-13 23:03:37 +0300

    исправление ошибки в open-list.js

--- source/js/open-list.js
+++ source/js/open-list.js
@@ -2,38 +2,30 @@ const vacancyList = document.querySelector(".vacancy-list");
 const advantagesList = document.querySelector(".advantages-list");
 
 const onVacancyListClick = (event) => {
-  if (event.target.matches(".button--open")) {
-    event.target.classList.remove("button--open");
-    event.target.classList.add("button--close");
+  if (event.target) {
+    event.target
+      .closest(".vacancy-list__item")
+      .querySelector(".vacancy-list__button")
+      .classList.toggle("button-more--close");
+
     event.target
       .closest(".vacancy-list__item")
       .querySelector(".conditions-list")
-      .classList.add("conditions-list--open");
-  } else {
-    event.target.classList.remove("button--close");
-    event.target.classList.add("button--open");
-    event.target
-      .closest(".vacancy-list__item")
-      .querySelector(".conditions-list")
-      .classList.remove("conditions-list--open");
+      .classList.toggle("conditions-list--open");
   }
 };
 
 const onAdvantagesListClick = (event) => {
-  if (event.target.matches(".button--open")) {
-    event.target.classList.remove("button--open");
-    event.target.classList.add("button--close");
+  if (event.target) {
+    event.target
+      .closest(".advantages-list__item")
+      .querySelector(".advantages-list__button")
+      .classList.toggle("button-more--close");
+
     event.target
       .closest(".advantages-list__item")
       .querySelector(".advantages-list__description")
-      .classList.add("advantages-list__description--open");
-  } else {
-    event.target.classList.remove("button--close");
-    event.target.classList.add("button--open");
-    event.target
-      .closest(".advantages-list__item")
-      .querySelector(".advantages-list__description")
-      .classList.remove("advantages-list__description--open");
+      .classList.toggle("advantages-list__description--open");
   }
 };
 
